79f9f76acd70aa63a7fc90a69b5dae8118e98ffc
Thales0820 <thalesaugustosilvalima@gmail.com>
2023-09-05 19:28:49 -0300

    Trabalho Pronto

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,8 @@ import React from "react";
 import {Header} from './components/Header';
 import {Card} from './components/Card';
 import {Footer} from "./components/Footer";
-//import Modal, {ModalBody, ModalHeader} from "./components/Modal";
 
 import banner from './imagens/banner.jpg';
-// import styles from './style.module.css';
 import './styles/global.css';
 import card1 from './imagens/card1.jpg'
 import card2 from './imagens/card2.jpg'
@@ -13,28 +11,7 @@ import card3 from './imagens/card3.jpg'
 import card4 from './imagens/card4.jpg'
 // npm install
 //npm run start
-// const requisicao = [
-//   {
-//     "id": 1,
-//     "titulo": "Missão Impossivel 2",
-//     "imagem": card1
-//   },
-//   {
-//     "id": 2,
-//     "titulo": "Missão Impossivel 3",
-//     "imagem": card2
-//   },
-//   {
-//     "id": 3,
-//     "titulo": "Missão Impossivel 5",
-//     "imagem": card3
-//   },
-//   {
-//     "id": 4,
-//     "titulo": "Missão Impossivel 7",
-//     "imagem": card4
-//   },
-// ]
+
 function App() {
   return (
     <>
@@ -54,55 +31,11 @@ function App() {
           alignItems: 'center',
           flexWrap: 'wrap'
         }}>
-        {/* {
-          requisicao.map((filme) => {
-            return (
-              <Card
-                imagem={filme.imagem}
-                titulo={filme.titulo} alterar texto para titulo no Card
-              />
-            )
-          })
-        } */}
-
           <Card imagem={card1} texto={"Missão Impossível 2"} />
           <Card imagem={card2} texto={"Missão Impossível 3"} />
           <Card imagem={card3} texto={"Missão Impossível 5"}/>
           <Card imagem={card4} texto={"Missão Impossível 7"}/>
       </div>
-      {/* <Modal>
-        <ModalHeader>
-          <h2>Chaves</h2>
-        </ModalHeader>
-        <ModalBody>
-          <p style={{
-            fontSize: '0.8rem',
-            marginBottom: 10
-          }}
-          >
-            04/09/2023
-          </p>
-          <div style={{
-            display: 'flex',
-            marginBottom: 10
-          }}
-          >
-            <p style={{
-            fontSize: '0.8rem',
-            color: '#b3b3b3'
-          }}
-          >
-            Direção:
-          </p>
-          <p style={{
-            fontSize: '0.8rem'
-          }}
-          >
-            Seu Madruga
-          </p>
-          </div>
-        </ModalBody>
-      </Modal> */}
       <Footer />
     </>
   );
